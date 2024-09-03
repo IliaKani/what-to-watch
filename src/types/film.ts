70fdbe74ea17fd14dbf1104ac895a64584bc5ffd
Ilia Kani <kanishchev.ilia@gmail.com>
2024-09-03 09:07:57 +0200

    added films

---
+++ src/types/film.ts
@@ -0,0 +1,19 @@
+export type TFilm = {
+  id: string;
+  name: string;
+  posterImage: string;
+  previewImage: string;
+  previewVideoLink: string;
+  backgroundImage: string;
+  backgroundColor: string;
+  videoLink: string;
+  description: string;
+  rating: number;
+  scoresCount: number;
+  director: string;
+  starring: string[];
+  runTime: number;
+  genre: string;
+  released: number;
+  isFavorite: boolean;
+};
---
+++ src/types/films.ts
@@ -0,0 +1,7 @@
+export type TFilms = {
+  id: string;
+  name: string;
+  previewImage: string;
+  previewVideoLink: string;
+  genre: string;
+};
