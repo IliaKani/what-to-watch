68824aca7f45aa024ea85cbf6650590e13a9e4ae
Ilia Kani <kanishchev.ilia@gmail.com>
2024-10-03 16:30:01 +0200

    change api actions

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import { AxiosInstance } from 'axios';
 import { TAppDispatch, TAppState } from '../types/state';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { dropToken, saveToken } from '../services/token';
@@ -9,12 +9,17 @@ import { TMyList } from '../types/my-list';
 import { TAddReview, TReviews } from '../types/reviews';
 import { TAuthData } from '../types/auth-data';
 import { APIRoute, NameSpace } from '../const';
-import { toast } from 'react-toastify';
 
 type TExtra = {
   dispatch: TAppDispatch;
   state: TAppState;
   extra: AxiosInstance;
+};
+
+const checkAuth = (api: AxiosInstance) => {
+  if (!api.defaults.headers.common['Authorization']) {
+    throw new Error('Sign in, please');
+  }
 };
 
 export const fetchFilmsAction = createAsyncThunk<TFilms[], undefined, TExtra>(
@@ -45,48 +50,29 @@ export const fetchPromoFilmAction = createAsyncThunk<TFilm, undefined, TExtra>(
 export const fetchFilmAction = createAsyncThunk<TFilm, TFilm['id'], TExtra>(
   `${NameSpace.Films}/fetchFilm`,
   async (id, { extra: api }) => {
-    try {
-      const { data } = await api.get<TFilm>(`${APIRoute.Films}/${id}`);
-      return data;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        toast.error(error.message);
-      }
-      throw error;
-    }
+    const { data } = await api.get<TFilm>(`${APIRoute.Films}/${id}`);
+    return data;
   }
 );
 
 export const fetchMyListAction = createAsyncThunk<TFilms[], undefined, TExtra>(
   `${NameSpace.Films}/fetchMyList`,
   async (_arg, { extra: api }) => {
-    try {
-      const { data } = await api.get<TFilms[]>(APIRoute.MyList);
-      return data;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        toast.error(error.message);
-      }
-      throw error;
-    }
+    checkAuth(api);
+    const { data } = await api.get<TFilms[]>(APIRoute.MyList);
+    return data;
   }
 );
 
 export const changeMyListAction = createAsyncThunk<TFilm, TMyList, TExtra>(
   `${NameSpace.Films}/changeMyList`,
   async ({ id, status }, { dispatch, extra: api }) => {
-    try {
-      const { data } = await api.post<TFilm>(
-        `${APIRoute.MyList}/${id}/${status}`
-      );
-      dispatch(fetchMyListAction());
-      return data;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        toast.error(error.message);
-      }
-      throw error;
-    }
+    checkAuth(api);
+    const { data } = await api.post<TFilm>(
+      `${APIRoute.MyList}/${id}/${status}`
+    );
+    dispatch(fetchMyListAction());
+    return data;
   }
 );
 
@@ -95,15 +81,8 @@ export const fetchReviewsAction = createAsyncThunk<
   TFilm['id'],
   TExtra
 >(`${NameSpace.Films}/fetchReviews`, async (id, { extra: api }) => {
-  try {
-    const { data } = await api.get<TReviews>(`${APIRoute.Reviews}/${id}`);
-    return data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      toast.error(error.message);
-    }
-    throw error;
-  }
+  const { data } = await api.get<TReviews>(`${APIRoute.Reviews}/${id}`);
+  return data;
 });
 
 export const addReviewAction = createAsyncThunk<
@@ -113,25 +92,20 @@ export const addReviewAction = createAsyncThunk<
 >(
   `${NameSpace.Films}/addReview`,
   async ({ reviewData, id }, { dispatch, extra: api }) => {
-    try {
-      const { data } = await api.post<TReviews>(
-        `${APIRoute.Reviews}/${id}`,
-        reviewData
-      );
-      dispatch(fetchReviewsAction(id));
-      return data;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        toast.error(error.message);
-      }
-      throw error;
-    }
+    checkAuth(api);
+    const { data } = await api.post<TReviews>(
+      `${APIRoute.Reviews}/${id}`,
+      reviewData
+    );
+    dispatch(fetchReviewsAction(id));
+    return data;
   }
 );
 
 export const checkAuthAction = createAsyncThunk<TUser, undefined, TExtra>(
   `${NameSpace.User}/checkAuth`,
   async (_arg, { extra: api }) => {
+    checkAuth(api);
     const { data } = await api.get<TUser>(APIRoute.Login);
     return data;
   }
@@ -140,33 +114,20 @@ export const checkAuthAction = createAsyncThunk<TUser, undefined, TExtra>(
 export const loginAction = createAsyncThunk<TUser, TAuthData, TExtra>(
   `${NameSpace.User}/login`,
   async ({ email, password }, { extra: api }) => {
-    try {
-      const { data } = await api.post<TUser>(APIRoute.Login, {
-        email,
-        password,
-      });
-      saveToken(data.token);
-      return data;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        toast.error(error.message);
-      }
-      throw error;
-    }
+    const { data } = await api.post<TUser>(APIRoute.Login, {
+      email,
+      password,
+    });
+    saveToken(data.token);
+    return data;
   }
 );
 
 export const logoutAction = createAsyncThunk<void, undefined, TExtra>(
   `${NameSpace.User}/logout`,
   async (_arg, { extra: api }) => {
-    try {
-      await api.delete(APIRoute.Logout);
-      dropToken();
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        toast.error(error.message);
-      }
-      throw error;
-    }
+    checkAuth(api);
+    await api.delete(APIRoute.Logout);
+    dropToken();
   }
 );
